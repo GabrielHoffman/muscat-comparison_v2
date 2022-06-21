dad4f0e725e159c7eb9c848ae5f6555c0957d682
Gabriel Hoffman <gabriel.hoffman@mssm.edu>
2022-06-21 15:59:07 -0400

    Create runing_analysis.sh

---
+++ runing_analysis.sh
@@ -0,0 +1,18 @@
+
+cd /sc/arion/projects/CommonMind/hoffman/muscat-comparison
+
+# clear results
+# rm -f plots/* results/*
+
+ml git
+git pull
+
+snakemake -j1 --rerun-incomplete
+
+# https://hoffmg01.u.hpc.mssm.edu/muscat-comparison/
+
+snakemake  --rerun-incomplete --jobs 200 --cluster 'bsub -q premium -R "rusage[mem=6000]" -R span[hosts=1] -W 6:00 -P acc_CommonMind -n 2' 
+
+
+
+
