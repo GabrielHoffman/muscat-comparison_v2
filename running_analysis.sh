27ec258d3eb43ba39801912070b115ac376989f0
Gabriel Hoffman <gabriel.hoffman@mssm.edu>
2022-06-21 16:22:29 -0400

    Update and rename runing_analysis.sh to running_analysis.sh

--- running_analysis.sh
+++ running_analysis.sh
@@ -2,7 +2,7 @@
 cd /sc/arion/projects/CommonMind/hoffman/muscat-comparison
 
 # clear results
-# rm -f plots/* results/*
+# rm -f plots/* results/* logs/*
 
 ml git
 git pull
@@ -10,6 +10,9 @@ git pull
 snakemake -j1 --rerun-incomplete
 
 # https://hoffmg01.u.hpc.mssm.edu/muscat-comparison/
+
+
+snakemake  --jobs 200 --cluster 'bsub -q premium -R "rusage[mem=6000]" -R span[hosts=1] -W 6
 
 snakemake  --rerun-incomplete --jobs 200 --cluster 'bsub -q premium -R "rusage[mem=6000]" -R span[hosts=1] -W 6:00 -P acc_CommonMind -n 2' 
 
