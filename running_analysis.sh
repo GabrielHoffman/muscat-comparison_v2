51e1c92bdf8c1b4acf5f7c7984d3f735bfb090d1
Gabriel Hoffman <gabriel.hoffman@mssm.edu>
2023-09-05 16:13:47 -0400

    Update running_analysis.sh

--- running_analysis.sh
+++ running_analysis.sh
@@ -12,7 +12,7 @@ snakemake -j1 --rerun-incomplete
 # https://hoffmg01.u.hpc.mssm.edu/muscat-comparison/
 
 
-snakemake  --jobs 200 --cluster 'bsub -q premium -R "rusage[mem=6000]" -R span[hosts=1] -W 6
+snakemake  --jobs 200 --cluster 'bsub -q premium -R "rusage[mem=6000]" -R span[hosts=1] -W 6'
 
 snakemake  --rerun-incomplete --jobs 200 --cluster 'bsub -q premium -R "rusage[mem=6000]" -R span[hosts=1] -W 6:00 -P acc_CommonMind -n 2' 
 
