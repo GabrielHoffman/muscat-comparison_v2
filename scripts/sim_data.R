362f6dd0228b5355606d4d9917e9976e48d5ea29
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2023-09-11 21:58:16 -0400

    update

--- scripts/sim_data.R
+++ scripts/sim_data.R
@@ -5,6 +5,7 @@ suppressMessages({
     library(scater)
     library(sctransform)
     library(SingleCellExperiment) 
+    library(HMP) 
 })
 
 # load data & simulation parameters
@@ -17,14 +18,40 @@ assignInNamespace( ".check_args_simData", function(u)
 
 sim <- simData(sce, 
     paired = FALSE, lfc = 0.5,
-    ng = nrow(sce), nc = sim_pars$nc,
+    ng = nrow(sce), nc = 10*sim_pars$nc,
     ns = sim_pars$ns, nk = sim_pars$nk,
     p_dd = sim_pars$p_dd, probs = sim_pars$probs,
     force=TRUE)
 
 sim <- sim[rowSums(counts(sim) > 0) >= 10, ]
-sim <- sim[sample(nrow(sim), min(nrow(sim), sim_pars$ng)), ]
 
+# don't subsample genes
+# sim <- sim[sample(nrow(sim), min(nrow(sim), sim_pars$ng)), ]
+
+# Downsample cell to get sim_pars$nc total
+# using Dirichlet-multinomial
+#############################
+
+alpha = 2
+tab = table(sim$sample_id, sim$cluster_id)
+countTarget = Dirichlet.multinomial(tab/10, c(alpha, alpha))
+
+df_grid = expand.grid(sid = levels(sim$sample_id), 
+                        cid = levels(sim$cluster_id))
+
+keep = lapply( seq(nrow(df_grid)), function(i){
+
+    keep = which( sim$sample_id == df_grid$sid[i] & sim$cluster_id == df_grid$cid[i])
+
+    ncells = countTarget[df_grid$sid[i],df_grid$cid[i]]
+
+    sample(keep, ncells)
+    })
+keep = sort(unlist(keep))
+
+sim = sim[,keep]
+
+# back to standard processing
 gi <- metadata(sim)$gene_info 
 gi <- dplyr::filter(gi, gene %in% rownames(sim))
 metadata(sim)$gene_info <- gi
@@ -36,3 +63,4 @@ assays(sim)$vstresiduals <- suppressWarnings(
     vst(counts(sim), show_progress = FALSE)$y)
 
 saveRDS(sim, args$sim)
+
