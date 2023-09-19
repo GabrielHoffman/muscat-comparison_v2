5abbbbbc74d3149b4b7ac1890d7dcde5c83aa790
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2023-09-18 22:03:51 -0400

    update

--- scripts/sim_data.R
+++ scripts/sim_data.R
@@ -51,7 +51,7 @@ rdmn = function(counts, alpha){
 
 if( k_scaling > 1){
     # overdispersion parameter 
-    alpha = 10
+    alpha = 1
 
     countTarget = rdmn(tab/k_scaling*2, rep(alpha, ncol(tab)))
 
