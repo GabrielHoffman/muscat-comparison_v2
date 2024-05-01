c753d3634542c92f740859fff2f2b3a4861e296c
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2024-05-01 14:57:47 -0400

    update

--- scripts/sim_data.R
+++ scripts/sim_data.R
@@ -17,44 +17,44 @@ assignInNamespace( ".check_args_simData", function(u)
 
 # simulate effect size heterogeneity
 # with a normal offset added to the logFC 
-.nb.replace <- function(cs, d, m, lfc = NULL, f = 1) {
-    n_gs <- length(d)
-    n_cs <- length(cs)
-    if (is.null(lfc)) {
-        lfc <- rep(0, n_gs)
-    } else {
-        lfc[lfc < 0] <- 0
-    }
-    names(lfc) = names(d)
+# .nb.replace <- function(cs, d, m, lfc = NULL, f = 1) {
+#     n_gs <- length(d)
+#     n_cs <- length(cs)
+#     if (is.null(lfc)) {
+#         lfc <- rep(0, n_gs)
+#     } else {
+#         lfc[lfc < 0] <- 0
+#     }
+#     names(lfc) = names(d)
 
-    # effect size heterogeneity for non-zero effects
-    if( any(lfc != 0) ){
-        i = lfc != 0
-        lfc[i] <- lfc[i] + rnorm(length(lfc[i]), 0, .1) 
-        # lfc[lfc < 0] <- 0
-    }
+#     # effect size heterogeneity for non-zero effects
+#     if( any(lfc != 0) ){
+#         i = lfc != 0
+#         lfc[i] <- lfc[i] + rnorm(length(lfc[i]), 0, .1) 
+#         # lfc[lfc < 0] <- 0
+#     }
 
-    fc <- f * (2 ^ lfc)
-    # cat fcs for all cells
-    fc <- rep(fc, each = n_cs)
+#     fc <- f * (2 ^ lfc)
+#     # cat fcs for all cells
+#     fc <- rep(fc, each = n_cs)
 
-    # cell-level heterogeneity when fc != 1
-    # i.e. lfc != 0
-    i = (fc != 1)
-    s = sqrt(rgamma(1,1, 1000))
-    fc[i] = 2^(log2(fc[i]) + rnorm(sum(i), 0, s))
-    fc = pmax(1, fc)
-    # plot(log(fc))
+#     # cell-level heterogeneity when fc != 1
+#     # i.e. lfc != 0
+#     i = (fc != 1)
+#     s = sqrt(rgamma(1,1, 1000))
+#     fc[i] = 2^(log2(fc[i]) + rnorm(sum(i), 0, s))
+#     fc = pmax(1, fc)
+#     # plot(log(fc))
 
-    ds <- rep(1/d, each = n_cs)
-    ms <- c(t(m[, cs])) * fc 
-    y <- rnbinom(n_gs * n_cs, size = ds, mu = ms)
-    y <- matrix(y, byrow = TRUE, 
-        nrow = n_gs, ncol = n_cs, 
-        dimnames = list(names(d), cs))
-    ms <- split(ms, rep(seq_len(nrow(m)), each = n_cs))
-    list(counts = y, means = ms)
-}
+#     ds <- rep(1/d, each = n_cs)
+#     ms <- c(t(m[, cs])) * fc 
+#     y <- rnbinom(n_gs * n_cs, size = ds, mu = ms)
+#     y <- matrix(y, byrow = TRUE, 
+#         nrow = n_gs, ncol = n_cs, 
+#         dimnames = list(names(d), cs))
+#     ms <- split(ms, rep(seq_len(nrow(m)), each = n_cs))
+#     list(counts = y, means = ms)
+# }
 
 # assignInNamespace(".nb", .nb.replace, ns="muscat")
 
@@ -86,7 +86,7 @@ if( k_scaling > 1){
         keep = which( sim$sample_id == df_grid$sid[i] & sim$cluster_id == df_grid$cid[i])
 
         target = tab[df_grid$sid[i],df_grid$cid[i]]/k_scaling
-        target = 20
+        # target = 20
 
         # sample cell counts from Negative Binomial 
         # Poisson if theta = Inf
@@ -136,6 +136,9 @@ sim2 <- logNormCounts(sim2)
 assays(sim2)$cpm <- calculateCPM(sim2)
 
 vst_values <- suppressWarnings(sctransform::vst(counts(sim2))$y)
+
+# vst() can fail for some genes, so only keep genes that succeed
+sim2 = sim2[rownames(vst_values,)]
 
 assays(sim2)$vstresiduals <- vst_values
 
